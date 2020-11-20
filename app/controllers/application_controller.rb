73ae440387fde3659de106015e7a0947e247bdcc
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-20 23:59:13 +0900

    ルーティング設定。views編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configre_permitted_parameters, if: :devise_controller?
+
+  def configre_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :location_id, :department_id, :bed, :service_years, :license, :company])
+  end
+  
 end
