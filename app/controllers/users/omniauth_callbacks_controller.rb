a1bb4a4972e73cb926d1ddfc34c7ad6e31ca6e53
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-21 12:05:27 +0900

    sns後の遷移先変更

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -40,5 +40,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def authorization
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else 
+      render template: 'devise/registrations/new'
+    end
   end
 end
