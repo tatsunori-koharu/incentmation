b9d1b7c666a8a6127b0029f481d696946c87fa57
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-21 23:06:27 +0900

    assocation設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+  has_many :articles
   belongs_to :location
   belongs_to :department
   has_many :sns_credentials
