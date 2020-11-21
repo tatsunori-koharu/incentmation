ea69881b79ca86557b410a82ab11ba09cdab0073
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-21 11:03:08 +0900

    association設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers: [:facebook, :google_oauth2]
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :location
   belongs_to :department
+  has_many :sns_credentials
 end
