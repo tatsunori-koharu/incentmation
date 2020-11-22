530cce53f5acc337f85b524ff0e6e2167275f11d
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-22 22:45:50 +0900

    画像投稿機能実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,21 +1,22 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function(){
+  document.addEventListener('DOMContentLoaded', function() {
     const ImageList = document.getElementById('image-list');
 
     const createImageHTML = (blob) => {
-      const imageElement = document.createElement('div');
-      imageElement.setAttribute('class', "image-element");
-      let imageElementNum = document.querySelectorAll('.image-element').length;
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
+
 
       const blobImage = document.createElement('img');
       blobImage.width = 300;
       blobImage.height = 180;
       blobImage.setAttribute('src', blob);
 
-      const inputHTML = document.createElement('input')
-      inputHTML.setAttribute('id', `article_image_${imageElementNum}`)
-      inputHTML.setAttribute('name', 'article[images][]')
-      inputHTML.setAttribute('type', 'file')
+      const inputHTML = document.createElement('input');
+      inputHTML.setAttribute('id', `article_image_${imageElementNum}`);
+      inputHTML.setAttribute('name', 'article[images][]');
+      inputHTML.setAttribute('type', 'file');
 
       imageElement.appendChild(blobImage);
       imageElement.appendChild(inputHTML);
@@ -29,10 +30,10 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       })
     };
 
-    document.getElementById('article_image').addEventListener('change', function(e){
+    document.getElementById('article_image').addEventListener('change', (e) => {
 
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
 
       createImageHTML(blob);
     });
