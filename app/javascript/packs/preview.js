51a4bf3bace395e5442a2a68e1133cbab18d6c32
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-22 22:21:43 +0900

    preview.js編集

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -4,21 +4,32 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 
     const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element");
+      let imageElementNum = document.querySelectorAll('.image-element').length;
+
       const blobImage = document.createElement('img');
       blobImage.width = 300;
       blobImage.height = 180;
       blobImage.setAttribute('src', blob);
 
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `article_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'article[images][]')
+      inputHTML.setAttribute('type', 'file')
+
       imageElement.appendChild(blobImage);
+      imageElement.appendChild(inputHTML);
       ImageList.appendChild(imageElement);
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob);
+      })
     };
 
     document.getElementById('article_image').addEventListener('change', function(e){
-      
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
 
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many_attached :image
+  has_many_attached :images
 
 
   with_options presence: true do
