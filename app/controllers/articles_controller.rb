7c3f6673e55c240d6f9ecc5c260b35045ce60dce
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-23 14:50:07 +0900

    article削除機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_articles, only: [:show, :edit, :update]
+  before_action :set_articles, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
@@ -29,6 +29,11 @@ class ArticlesController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
