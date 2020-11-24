31580d9137a635842494068e27c10f985aa00e9c
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-24 18:48:08 +0900

    articleテストコード記述

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :article do
     title    {Faker::Lorem.characters(number: 10)}
     content  {Faker::Lorem.sentences}
+    association :user
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @article = FactoryBot.build(:article)
+  end
+
+  describe "article登録" do
+    context "articleの登録ができる" do
+      it "titleとcontentを入力すれば登録できる" do
+        expect(@article).to be_valid
+      end
+    end
+    context "articleの登録ができない" do
+      it "titleが空では登録できない" do
+        @article.title = nil
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Title can't be blank")
+      end
+      it "titleが41字以上だと登録できない" do
+        @article.title = "a" * 41
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Title is too long (maximum is 40 characters)")
+      end
+      it "contentが空では登録できない" do
+        @article.content = nil
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Content can't be blank")
+      end
+    end
+  end
 end
