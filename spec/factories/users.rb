277ea713f95133c263d75bd43677cfd05be78a43
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-24 18:10:09 +0900

    userモデルテスト実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :user do
     nickname               {Faker::Name.name}
     email                  {Faker::Internet.free_email}
-    password = {Faker::Internet.password(min_length: 8)}
+    password = Faker::Internet.password(min_length: 6)
     password               {password}
     password_confirmation  {password}
-    location_id            {2}
+    location_id            { "2" }
     department_id          {2}
     bed                    {22}
     service_years          {33}
