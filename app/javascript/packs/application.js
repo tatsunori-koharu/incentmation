90ff7ab53ba143a6d7dc95b3eed16fd37a131d59
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-26 17:04:38 +0900

    bootstrap導入修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,10 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
+//= require 'jquery'
 require("./preview")
-require("popper")
-require("bootstrap-sprockets")
+//=require "popper"
+//=require "bootstrap-sprockets"
 
 import 'bootstrap'
 import '../src/application.scss'
