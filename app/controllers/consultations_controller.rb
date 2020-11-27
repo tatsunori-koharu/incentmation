36610d1722f0ba8585dcf9a4f91d8c5998118921
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-27 18:25:04 +0900

    consultationコメント機能実装

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,4 +1,5 @@
 class ConsultationsController < ApplicationController
+  before_action :set_consultation, only: :[:show, :edit, :update, :destroy]
 
   def index
     @consultations = Consultation.all.order("created_at DESC")
@@ -18,9 +19,24 @@ class ConsultationsController < ApplicationController
   end
 
   def show
+    @consultation_comment = ConsultationComment.new
+    @consultation_comments = @consultation.consultation_comments.includes(:user)
   end
 
   def edit
+  end
+
+  def update
+    if @consultation.update(consultation_params)
+      redirect_to consultations_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @consultation.destroy
+    redirect_to consultations_path
   end
 
   private
@@ -28,5 +44,9 @@ class ConsultationsController < ApplicationController
   def consultation_params
     params.reqire(:consultation).parmit(:title, :cotent).marge(user_id: current_user.id)
   end
-  
+
+  def set_consultation
+    @consultation = Consultation.find(params[:id])
+  end
+
 end
