d5d1f83c9528f5c1d4bbb144d1a1e3524dd4d97a
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-27 17:51:42 +0900

    トップページ修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def top
     @articles = Article.all.limit(5).order("created_at DESC")
+    @consultations = Consultation.all.limit(5).order("created_at DESC")
   end
 end
