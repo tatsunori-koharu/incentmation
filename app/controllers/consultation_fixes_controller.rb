db0c17e363d67d38d089a636b289697664015aa5
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-27 23:36:32 +0900

    Update consultation_fixes_controller.rb

--- app/controllers/consultation_fixes_controller.rb
+++ app/controllers/consultation_fixes_controller.rb
@@ -1,2 +1,20 @@
 class ConsultationFixesController < ApplicationController
+  before_action :set_fix
+
+  def create
+    cousultation = Consultation.find(params[:consultation_id])
+    consultation_fix = ConsultationFix.create(user_id: current_user.id, consultation_id: cousultation.id)
+  end
+
+  def destroy
+    cousultation = Consultation.find(params[:consultation_id])
+    consultation_fix = ConsultationFix.find_by(user_id: current_user.id, consultation_id: cousultation.id)
+    consultation_fix.destroy
+  end
+
+  private
+
+  def set_fix
+    @cousultation = Consultation.find(params[:consultation_id])
+  end
 end
