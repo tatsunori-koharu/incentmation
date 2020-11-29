ef7c12bfe41f39bddb0d176524f1bb2d0ac2caf7
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-29 22:56:54 +0900

    promotionモデル生成

---
+++ db/migrate/20201129135602_create_promotions.rb
@@ -0,0 +1,8 @@
+class CreatePromotions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :promotions do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/promotions.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :promotion do
+    
+  end
+end
