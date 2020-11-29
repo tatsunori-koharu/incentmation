f9c0a6f6d1452eda6302f97ad5318fa3e60e62ce
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-29 23:19:54 +0900

    アソシエーション設定

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,11 @@
 class Promotion < ApplicationRecord
+  belongs_to :user
+  has_many_attached :images
+  has_one_attached :move
+
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :content
+  end
+  
 end
