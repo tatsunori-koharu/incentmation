3a8b16658a567cb9399ba93dce017f62c56159de
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-30 11:14:46 +0900

    promotion_commentコントローラー生成

---
+++ app/controllers/promotion_comments_controller.rb
@@ -0,0 +1,14 @@
+class PromotionCommentsController < ApplicationController
+
+  def create
+    promotion_comment = PromotionComment.create(promotion_comment_params)
+    redirect_to "/promotion/#{promotion_comment.promotion.id}"
+  end
+
+  private
+
+  def promotion_comment_params
+    params.require(:promotion_comment).permit(:comment).merge(user_id: current_user.id, promotion_id: params[:promotion_id])
+  end
+  
+end
