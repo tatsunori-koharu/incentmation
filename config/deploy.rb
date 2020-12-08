e5379a67e9031f6fcf75627fe86f916c22d2887d
tatsunori-koharu <tats208919ch@gmail.com>
2020-12-08 18:00:29 +0900

    capistrano導入

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+# capistranoのバージョン
+lock '3.14.1'
+
+# Capistranoのログの表示に利用する
+set :application, 'incentmation'
+
+# pull指定
+set :repo_url,  'git@github.com:tatsunori-koharu/incentmation.git'
+
+# 共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/koharu0930.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
