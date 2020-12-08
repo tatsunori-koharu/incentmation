5c2187e5d11688afaaa66a08d252c70136690e33
tatsunori-koharu <tats208919ch@gmail.com>
2020-12-08 16:07:26 +0900

    unicornインストール

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :location_id, :department_id, :bed, :service_years, :license, :company])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :location_id, :department_id, :bed, :service_years, :license, :company])    
   end
   
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
 end
