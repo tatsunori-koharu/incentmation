565a2e7a3c765b4c8dbd4a23f3c95ce8a8758b8d
tatsunori-koharu <tats208919ch@gmail.com>
2020-12-22 12:27:29 +0900

    categoryカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_054254) do
+ActiveRecord::Schema.define(version: 2020_12_22_032253) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_054254) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -101,6 +102,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_054254) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
     t.index ["user_id"], name: "index_consultations_on_user_id"
   end
 
@@ -138,6 +140,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_054254) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
     t.index ["user_id"], name: "index_promotions_on_user_id"
   end
 
