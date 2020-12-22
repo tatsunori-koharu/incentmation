e6260b812901d27a8773be61c746dfaf39c62bf5
tatsunori-koharu <tats208919ch@gmail.com>
2020-12-22 11:58:45 +0900

    カテゴリーactivehash生成

---
+++ app/models/category.rb
@@ -0,0 +1,11 @@
+class category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '滅菌' },
+    { id: 3, name: '洗浄' },
+    { id: 4, name: '包装' },
+    { id: 5, name: '乾燥' },
+    { id: 6, name: '組み立て' },
+    { id: 7, name: '保管' },
+  ]
+  end
