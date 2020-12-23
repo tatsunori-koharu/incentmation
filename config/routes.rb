dfaad79599fe1a7c6271ee181fd6862b1540aa90
tatsunori-koharu <tats208919ch@gmail.com>
2020-12-23 13:57:41 +0900

    search/routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
   root to: "home#top"
+  get "home/search"
   devise_for :users, controllers: {
           omniauth_callbacks: 'users/omniauth_callbacks',
           registrations: 'users/registrations'
