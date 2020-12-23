4c25cd89d75b3c01a995cabe9e2a3d1245cdd225
tatsunori-koharu <tats208919ch@gmail.com>
2020-12-23 14:14:29 +0900

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,20 @@
 class HomeController < ApplicationController
+
+  before_action :set_search, only [:top, :search]
+
   def top
     @articles = Article.all.limit(5).order("created_at DESC")
     @consultations = Consultation.all.limit(5).order("created_at DESC")
     @promotions = Promotion.all.limit(5).order("created_at DESC")
+  end
+
+  def search
+    @article_results = @a.result.includes(:category)
+  end
+
+  private
+
+  def set_search
+    @a = Article.rensack(params[:q])
   end
 end
