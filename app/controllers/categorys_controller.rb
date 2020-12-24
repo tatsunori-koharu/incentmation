3859858060fefb2de84337423c1030193adf8e07
tatsunori-koharu <tats208919ch@gmail.com>
2020-12-24 14:21:40 +0900

    Update categorys_controller.rb

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,4 +1,7 @@
 class CategorysController < ApplicationController
   def show
+    @article_categorys = Category.find(params[:id]).articles
+    @consultation_categorys = Category.find(params[:id]).consultations
+    @promotion_categorys = Category.find(params[:id]).promotions
   end
 end
