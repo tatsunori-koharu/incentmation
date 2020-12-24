44986a34f23ae4a4a3f603da7dab734ae4d087ae
tatsunori-koharu <tats208919ch@gmail.com>
2020-12-24 17:15:35 +0900

    カテゴリー分け実装

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,5 +1,6 @@
 class CategorysController < ApplicationController
   def show
+    @category = Category.find(params[:id])
     @article_categorys = Category.find(params[:id]).articles
     @consultation_categorys = Category.find(params[:id]).consultations
     @promotion_categorys = Category.find(params[:id]).promotions
