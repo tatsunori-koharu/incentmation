d26fc0b5374a6fd7d5fd7c8f479fb997b0b4bef0
tatsunori-koharu <tats208919ch@gmail.com>
2021-01-01 22:36:21 +0900

    Header.js生成

---
+++ app/javascript/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+import UserNav from "./UserNav"
+class Header extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Header
