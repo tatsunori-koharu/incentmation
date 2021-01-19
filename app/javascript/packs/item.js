9eb8262a2691300df49e399906c449756d9ced58
tatsunori-koharu <tats208919ch@gmail.com>
2021-01-19 18:21:49 +0900

    article/index script追加

--- app/javascript/packs/item.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  $('.item_<%= article.id %>').on('mouseover', function(e) {
-    $('.item-img').slideToggle(0);
-    $('.item-text_<%= article.id %>').slideToggle(0);
-  });
-})
