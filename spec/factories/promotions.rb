f7d86eeec3eca36d21a9bea9ee328f7a6c081156
tatsunori-koharu <tats208919ch@gmail.com>
2021-01-21 22:52:18 +0900

    promotionテストコード作成

--- spec/factories/promotions.rb
+++ spec/factories/promotions.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :promotion do
-    
+    name            {Faker::Lorem.characters(number: 10)}
+    content         {Faker::Lorem.sentences}
+    category_id     {2}
+    association :user
   end
 end
