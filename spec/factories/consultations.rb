5bd3b458c09f62557c861b39de183d6e437c04ed
tatsunori-koharu <tats208919ch@gmail.com>
2021-01-21 22:45:15 +0900

    consultationテストコード作成

--- spec/factories/consultations.rb
+++ spec/factories/consultations.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :consultation do
-    
+    title           {Faker::Lorem.characters(number: 10)}
+    content         {Faker::Lorem.sentences}
+    category_id     {2}
+    association :user
   end
 end
