3a2f4ec3163a663f02d8310c570c040d4217f64b
tatsunori-koharu <tats208919ch@gmail.com>
2021-01-21 22:42:10 +0900

    articleテストコード作成

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -15,17 +15,22 @@ RSpec.describe Article, type: :model do
       it "titleが空では登録できない" do
         @article.title = nil
         @article.valid?
-        expect(@article.errors.full_messages).to include("Title can't be blank")
+        expect(@article.errors.full_messages).to include("タイトルを入力してください")
       end
       it "titleが41字以上だと登録できない" do
         @article.title = "a" * 41
         @article.valid?
-        expect(@article.errors.full_messages).to include("Title is too long (maximum is 40 characters)")
+        expect(@article.errors.full_messages).to include("タイトルは40文字以内で入力してください")
       end
       it "contentが空では登録できない" do
         @article.content = nil
         @article.valid?
-        expect(@article.errors.full_messages).to include("Content can't be blank")
+        expect(@article.errors.full_messages).to include("内容を入力してください")
+      end
+      it "categoryが「ーーー」なら登録できない" do
+        @article.category_id = 1
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Categoryは1以外の値にしてください")
       end
     end
   end
