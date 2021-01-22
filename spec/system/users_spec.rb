3c744b6a01225a7e2baaee5a51f484a2f0511a5d
tatsunori-koharu <tats208919ch@gmail.com>
2021-01-22 22:56:47 +0900

    Create users_spec.rb

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  pending "add some scenarios (or delete) #{__FILE__}"
+end
