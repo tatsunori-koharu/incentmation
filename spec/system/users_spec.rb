5d23a463507cbb140cf4a60e43339441099bce34
tatsunori-koharu <tats208919ch@gmail.com>
2021-01-23 23:50:32 +0900

    usersテストコード完成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -73,6 +73,50 @@ RSpec.describe "Users", type: :system do
       expect(current_path).to eq "/users"
     end
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+end
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができること' do
+    it '保存されているユーザーの情報と合致すればログインできる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # 正しいユーザー情報を入力する
+      fill_in 'Eメール', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
+      # カーソルを合わせてクリックするとログアウトボタンが表示されることを確認する
+      expect(
+        find(".user-icon").find(".user-menu").click
+      ).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができないこと' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'Eメール', with: ""
+      fill_in 'パスワード', with: ""
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq new_user_session_path
+    end
+  end
 end
